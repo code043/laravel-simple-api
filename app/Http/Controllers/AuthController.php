85d3787782a05f4c1779943dd61dc46239ad7783
code043 <procoder043@gmail.com>
2024-11-20 15:27:35 -0300

    Registering a user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(){
-        return 'register';
+    public function register(Request $request){
+        $validate = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:3'
+        ]);
+        $user = User::create($validate);
+        $token = $user->createToken($validate['name'])->plainTextToken;
+        return response()->json([
+            'message' => 'User has beed registered!',
+            'user' => $user,
+            'token' => $token
+        ],201);
     }
     public function login(){
         return 'login';
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::apiResource('posts', PostController::class);
 
